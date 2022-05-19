189f202937e4f9f3780d4035b793237e2eb26dc9
Alice Bryer <alicebryer123@gmail.com>
2022-05-19 21:01:12 +0100

    api data fetched

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,25 @@
 console.log("test");
+
+// video API
+// getting the data
+
+fetch(
+  "https://www.scorebat.com/video-api/v3/feed/?token=[MTkxNzZfMTY1MjY5MzE4Nl8zYmM4Y2NkMGEzMmVkN2MwYWRlMzBkZTk1Mjg0NGVmZmIwMTZmMzMy]"
+)
+  .then((response) => response.json())
+  .then((data) => filteredItems(data));
+
+//   getting the items from the data
+
+const filteredItems = (data) => {
+  const englishTeams = data.response.filter((item) =>
+    item.competition.includes("ENGLAND")
+  );
+  console.log(englishTeams);
+};
+
+// changing the links in HTML
+
+var title = document.getElementById(videoTitle);
+var matchDate = document.getElementById(date);
+var url = document.getElementById(VideoURL);
