65784fddf7b8e4f428a8de52d242faa7bec0b10c
Alice Bryer <alicebryer123@gmail.com>
2022-05-23 16:03:04 +0100

    local storage function updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,33 @@
-function store() {
-  var inputTeams = document.getElementById("ftb");
-  localStorage.setItem("ftb", inputTeams.value);
-  console.log("inputTeams.value");
-}
+// Saving the search into local storage
 
-var storedTeams = localStorage.getItem("ftb");
-console.log("test");
+function store() {
+  var inputTeams = document.getElementById("favouriteTeams");
+  localStorage.setItem("favouriteTeams", inputTeams.value);
+}
+var storedTeam = localStorage.getItem("favouriteTeams");
+
+console.log(storedTeam);
+
+// append the search onto recent searches list
+
+const list = document.getElementById("demo");
+const newData = storedTeam;
+const entry = document.createElement("li");
+entry.appendChild(document.createTextNode(newData));
+list.appendChild(entry);
+
+// re-render the data from API when clicking on a recent search
+
+// clear button to clear all local storages - on click event - localStorage.clear ();
 
 // // video API
-// // getting the data
+
+// on document.ready - render the videos
+// // fetching the data
+
+// let titles = [];
+let dates = [];
+let links = [];
 
 fetch(
   "https://www.scorebat.com/video-api/v3/feed/?token=[MTkxNzZfMTY1MjY5MzE4Nl8zYmM4Y2NkMGEzMmVkN2MwYWRlMzBkZTk1Mjg0NGVmZmIwMTZmMzMy]"
@@ -16,7 +35,7 @@ fetch(
   .then((response) => response.json())
   .then((data) => filteredItems(data));
 
-//   getting the items from the data
+//   filtering the data
 
 const filteredItems = (data) => {
   const englishTeams = data.response.filter((item) =>
@@ -26,7 +45,17 @@ const filteredItems = (data) => {
   return englishTeams;
 };
 
-// API Getting I.D's from form input
+// // const titles = json.map((filteredItems) => filteredItems.title);
+// // console.log(titles);
+
+// const titles = englishTeams.map((englishTeams) => englishTeams.title);
+// console.log(titles);
+
+// Looping/mapping through the data to get the VideoURL x3
+
+// appending the URL into the HTML
+
+// API Getting I.D's from form input - use input to search through the response
 
 fetch("https://v3.football.api-sports.io/teams?league=39&season=2021", {
   method: "GET",
@@ -49,7 +78,7 @@ fetch("https://v3.football.api-sports.io/teams?league=39&season=2021", {
 
 const getFixtures = async () => {
   const res = await fetch(
-    "https://v3.football.api-sports.io/fixtures?team=50&season=2021&status=NS",
+    "https://v3.football.api-sports.io/fixtures?team=40&season=2021&status=FT",
     {
       method: "GET",
       headers: {
